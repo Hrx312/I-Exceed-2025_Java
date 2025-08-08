4428b1c83f9aefbf34f473b70c58b78c1d8af3c2
Hrx312 <harshraj312312@gmail.com>
2025-08-08 13:51:52 +0530

    Add files via upload

---
+++ day5/demothread4.java
@@ -0,0 +1,26 @@
+class sample extends Thread
+{
+    public void run()
+	{
+	  for(var i=0;i<10;i++)
+	   {
+	     try{
+	      System.out.println(i);
+	      new Thread(this).sleep(1000);
+	     }catch(Exception d){}
+	   }
+ 
+	}
+}
+ 
+class demothread6
+{
+    public static void main(String ads[]) throws InterruptedException
+	{
+	   sample obj=new sample();
+	   obj.start();
+	  
+		  
+		}
+ 
+	}
