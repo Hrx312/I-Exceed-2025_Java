5077a58bcb63af9cbaa5093107583c2f59511f94
Hrx312 <harshraj312312@gmail.com>
2025-08-18 14:01:24 +0530

    Add files via upload

---
+++ day 10/demovar3.js
@@ -0,0 +1,25 @@
+ 
+function display()
+{
+    var x=10;
+    function display1()
+        {
+            var y=20;
+            console.log(x);
+            console.log(y);
+        }
+        //console.log(y);
+    display1();
+}
+display();
+ 
+ 
+// Hositing
+console.log("Prior to assigning",a);
+ 
+var a=100;
+ 
+console.log("after assigning",a);
+ 
+ 
+ 
