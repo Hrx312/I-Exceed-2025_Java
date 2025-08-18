a589c7a1dd1d8f0fd972c42815588fc73bce0cf1
Hrx312 <harshraj312312@gmail.com>
2025-08-18 16:20:11 +0530

    Update demodata1.js

--- day 10/demodata1.js
+++ day 10/demodata1.js
@@ -3,5 +3,21 @@ let name="Harsh";
 let name1='Raj';
 let name2='x';
 console.log(`email is  ${name1+"@"+"i-exceed.com"} `);
-let val4=undefined;
-console.log(val4);
+let val4=1000;
+let val5=2000;
+console.log(Math.max(val4,val5));
+console.log(Math.min(val4,val5));
+console.log(Math.round());
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MIN_SAFE_INTEGER);
+console.log(Number.POSITIVE_INFINITY);
+console.log(Number.NEGATIVE_INFINITY);
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+console.log(Number.EPSILON);
+console.log(Number.NaN);
+
+
+
+
+
