4fb3daa0317be674558b84b3d1ef0ecc0d9b9b1a
Hrx312 <harshraj312312@gmail.com>
2025-08-19 17:36:31 +0530

    Add files via upload

---
+++ day11/boolnum.js
@@ -0,0 +1,58 @@
+let x="123";
+console.log(Number(x));
+console.log(typeof x);
+ 
+let y=null;
+console.log(y);
+console.log(typeof y);
+console.log(Number(y));
+ 
+let z=undefined;
+console.log(z);
+console.log(typeof z);
+console.log(Number(z));
+ 
+let a=false;
+console.log(typeof a);
+console.log(Number(a));
+ 
+let a1=true;
+console.log(typeof a1);
+console.log(Number(a1));
+ 
+let a2="hi";
+console.log(Number(a2));
+
+console.log(false && true);
+console.log(null && true);
+console.log(NaN && true);
+console.log(undefined && true);
+console.log(0 && true);
+console.log(Number(""));
+ 
+// Boolean of 0 ,null, undefined ,NaN resultant with false
+let res=Boolean(1);
+console.log(res)
+
+let value=true
+console.log(String(value))
+console.log(String(null))
+console.log(String(undefined))
+console.log(String(false))
+
+"A">"A";
+console.log(null==undefined)
+
+"Glow" > "Glee"
+2>1
+"2" < 1
+"123"<"20"
+"123"<20
+"123">20
+console.log(true==1)
+console.log(false==0)
+console.log(true=="false")
+
+
+
+ 
